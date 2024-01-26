159b8210cbe4c9aafde171bdc3a1d45c8cd834a2
Gi Pisu <gino03.pisu@gmail.com>
2024-01-26 23:41:14 +0100

    Rechtsklick

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,10 +8,16 @@ public class Weapon : MonoBehaviour
     public Transform shootPoint;
     public float shots = 0;
     public float magazine = 10;
+    public float Timer = 0;
+    public float rShots;
+    public float rMagazine;
+    public float holdTime = 1;
 
     bool isreloading;
-   
-    
+    bool drei;
+    bool rclick;
+
+
 
 
 
@@ -24,12 +30,26 @@ public class Weapon : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0) && !isreloading)
         {
-            
+
             Shoot();
-            
+
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= holdTime) 
+            {
+                drei = true;
+                Timer = 0;
+            }
+
         }
 
-   
+        if (Input.GetKeyUp(KeyCode.Mouse1) && drei) //Rechtsklick 3fach Schuss
+        {
+            StartCoroutine(RClick(3));
+            drei = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -37,7 +57,7 @@ public class Weapon : MonoBehaviour
 
             StartCoroutine(Reload());
         }
-     
+
 
         if (shots == magazine)
         {
@@ -55,6 +75,14 @@ public class Weapon : MonoBehaviour
         shots++;
 
     }
+
+    void rShoot()
+    {
+        GameObject bulletInstance = Instantiate(bullet, shootPoint.position, transform.rotation);
+        bulletInstance.GetComponent<Rigidbody2D>().AddForce(-transform.right * 1000);
+        Destroy(bulletInstance, 5);
+
+    }
     IEnumerator Reload()
     {
         shots = 0;
@@ -62,5 +90,13 @@ public class Weapon : MonoBehaviour
         isreloading = false;
 
     }
-   
+    IEnumerator RClick(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            rShoot();
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
 }
