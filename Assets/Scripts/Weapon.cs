ab414cc43c49889b649a3484c46c1657cec0dcfe
Gi Pisu <gino03.pisu@gmail.com>
2024-01-26 22:02:39 +0100

    gf
    ffdd

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,16 +6,63 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bullet;
     public Transform shootPoint;
+    public float shots = 0;
+    public float magazine = 10;
+
+    bool isreloading;
+    
    
     // Update is called once per frame
+
+    //Wenn Linksklick dann nicht dauerschieﬂen
+
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isreloading)
+        {
             Shoot();
+         
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+           StartCoroutine (Repetieren());
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isreloading = true;
+
+            StartCoroutine (Reload());
+        }
+
+        if (shots == magazine)
+        {
+            isreloading = true;
+           
+           StartCoroutine (Reload());
+        }
     }
-    public virtual void Shoot()
+      
+    void Shoot()
     {
         GameObject bulletInstance = Instantiate(bullet, shootPoint.position,transform.rotation);
         bulletInstance.GetComponent<Rigidbody2D>().AddForce(-transform.right * 1000);
+        Destroy (bulletInstance, 5);
+        shots++;
+        
     }
+    IEnumerator Reload()
+    {
+        shots = 0;
+        yield return new WaitForSeconds(3);
+        isreloading = false;
+       
+    }
+    IEnumerator Repetieren()
+            {
+                yield return new WaitForSeconds(0.5f);
+                 Shoot();
+            }
+           
 }
