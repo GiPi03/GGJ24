baf64809dd476d3b4a2e8b3af423b50f3594b225
Dnold <dnold.png@gmail.com>
2024-01-27 10:47:56 +0100

    Stuff
    lol

--- Assets/Scripts/WeaponMovement.cs
+++ Assets/Scripts/WeaponMovement.cs
@@ -16,6 +16,15 @@ public class WeaponMovement : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get mouse position
         Vector2 mouseDir = (Vector3)mousePos - transform.position; // Get weapon position
         float angle = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg; // Get angle
+        Debug.Log(angle);
+        if(angle > 60f && angle < 150f) // If angle is between 60 and 120
+        {
+            weapon.GetComponent<SpriteRenderer>().sortingOrder = 4; // Set weapon sorting order to 4
+        }
+        else
+        {
+            weapon.GetComponent<SpriteRenderer>().sortingOrder = 6; // Set weapon sorting order to 6
+        }
         weapon.rotation = Quaternion.Euler(0,0,angle-180); // Set rotation
     }
 }
