e58060f158b0889312be907dd90f3b56365d79c4
Gi Pisu <gino03.pisu@gmail.com>
2024-01-28 14:01:26 +0100

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -75,14 +75,12 @@ public class GameManager : MonoBehaviour
         
         mapValue = mapGenerator.GenerateMap(i);
         StartCoroutine(UpdateCollider());
-        
+        AstarPath.active.Scan();
         
         Destroy(player);
         player = SpawnPlayer();
         spawnEnemy = Random.Range(minEnemy, maxEnemy);
-        
         SpawnEnemies();
-
         allEnemyDead = false;
         i++;
     }
@@ -113,7 +111,6 @@ public class GameManager : MonoBehaviour
             if (mapGenerator.wallTilemap != null)
             {
                 mapGenerator.wallTilemap.GetComponent<TilemapCollider2D>().enabled = true;
-                AstarPath.active.Scan();
             }
         }
     }
